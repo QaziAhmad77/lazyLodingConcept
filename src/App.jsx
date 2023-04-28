ef3873ffe7631269ad1d105a4408e0804864a01d
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-04-28 15:07:34 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -31,4 +31,24 @@ export default App;
 component and it cause delay for another components also so 
 here comes the concept of lazy loading where if first render
 the component which has small data and then it shows us a 
-message whatever we want and after fectcing data it display. */
+message whatever we want and after fectcing data it display.
+
+Lazy loading is a technique used in React.js to improve the 
+performance of web applications by loading components only 
+when they are needed. When lazy loading is implemented, 
+instead of loading all the components at once, only the 
+components that are required at a given time are loaded.
+
+Lazy loading is typically used for large components that 
+might not be used immediately when a user visits a website. 
+For example, if a website has a large image gallery, it 
+might not make sense to load all the images at once when 
+a user first visits the page. Instead, the images can be 
+loaded as the user scrolls down the page, which can 
+significantly improve the page load time and overall performance.
+In React.js, lazy loading is achieved using the React.lazy()
+ function and Suspense component. The React.lazy() function 
+ allows you to load a component lazily, while the Suspense 
+ component allows you to display a fallback content while 
+ the lazy-loaded component is being loaded.
+*/
